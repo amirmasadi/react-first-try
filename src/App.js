55c663ed44597844badbb5bee82cf1a685859f98
amirasadi <a.m.asadi2001@gmail.com>
2021-07-25 12:19:50 +0430

    some additional changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.rtl.min.css'
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand me-auto" href="#">Navbar</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
